fa340b757a2a92e89b84a301c50b4f7f40602200
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2022-01-14 15:37:11 -0700

    Fix footer css class names

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,11 +4,11 @@ import logo from '../../assets/Group 262.png'
 const Footer = () => {
   return (
     <footer>
-      <div className="footerSection">
+      <div className="footer-section">
         <h2>South Carolina Revenue and Fiscal Affairs Office</h2>
         <img src={logo} alt="SCRFA logo"/>
       </div>
-      <div className="footerSection">
+      <div className="footer-section">
         <ul>
           <li>Privacy Statements</li>
           <li>FOIA</li>
@@ -18,18 +18,18 @@ const Footer = () => {
           <li>Contact Us</li>
         </ul>
       </div>
-      <div className="footerSection">
-        <div className="officeAddress">
+      <div className="footer-section">
+        <div className="office-address">
           <h4>Main Office</h4>
           <p>1000 Assembly St. Rembert Dennis Building, Suite 421</p>
           <p>Columbia, SC 29201</p>
         </div>
-        <div className="officeAddress">
+        <div className="office-address">
           <h4>Geodetic Survey Section</h4>
           <p>S Geology Rd</p>
           <p>Columbia, SC 29212</p>
         </div>
-        <div className="officeAddress">
+        <div className="office-address">
           <h4>Health and Demographics Division</h4>
           <p>1000 Assembly St. Rembert Dennis Building, Suite 420</p>
           <p>Columbia, SC 29201</p>
