a7bfcf37a56af0ac0884f01db0f8dfea070a8428
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2022-01-16 15:13:58 -0700

    Add navbar component first iteration

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Footer from './Components/Footer/Footer'
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Header/>
       <CatagoryIcons/>
       <PopularServices/>
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,9 +1,24 @@
 import './NavBar.css'
+import logo from '../../assets/RFA Logo Final.png'
 
 const NavBar = () => {
   return (
     <nav>
-    
+      <div className="nav-header">
+        <button>Menu</button>
+        <img src={logo} alt="SCRFA logo"/>
+        <div className="nav-title">
+          <p>South Carolina</p>
+          <p>Revenue and Fiscal Affairs Office</p>
+          <p>Transforming data into solutions for South Carolina</p>
+        </div>
+      </div>
+      <div className="nav-links">
+        <a>About Us</a>
+        <a>Events</a>
+        <a>Boards & Commitees</a>
+        <button>Search</button>
+      </div>  
     </nav>
   )
 }
